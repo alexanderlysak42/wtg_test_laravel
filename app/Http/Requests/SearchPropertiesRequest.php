d357f5035b7153ef47d4263ef0b9677e2768b8d7
Alexander Lysak <webdev.alex42@gmail.com>
2026-09-05 09:27:27 +0300

    Feat - add form requests

---
+++ app/Http/Requests/SearchPropertiesRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchPropertiesRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'city' => ['nullable', 'string', 'max:191'],
+            'check_in' => ['required', 'date_format:Y-m-d'],
+            'check_out' => ['required', 'date_format:Y-m-d', 'after:check_in'],
+            'guests' => ['nullable', 'integer', 'min:1'],
+            'per_page' => ['nullable', 'integer', 'min:1', 'max:100'],
+        ];
+    }
+}
---
+++ app/Http/Requests/StoreReservationRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreReservationRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'client_reference' => ['required', 'string', 'max:191'],
+            'customer_name' => ['required', 'string', 'max:191'],
+            'customer_email' => ['required', 'email', 'max:191'],
+        ];
+    }
+}
